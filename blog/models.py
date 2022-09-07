3e6a4268bca8bdd99972a29530b68055a73ca86a
Stuffy33 <eric96an@gmail.com>
2022-09-07 19:04:20 +0000

    add initial models3

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,7 @@ STATUS = ((0, "Draft"), (1, "Published"))
 class Post (models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_post")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
     featured_image CloudinaryField('image', default='placeholder')
@@ -17,7 +17,7 @@ class Post (models.Model):
     likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
 
     class Meta:
-        ordering = ['-created_on']
+        ordering = ["-created_on"]
 
     def __str__(self):
         return self.title
@@ -26,7 +26,6 @@ class Post (models.Model):
         return self.likes.count()
 
 class Comment(models.Model):
-
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     name = models.CharField(max_length=80)
     email = models.EmailField()
@@ -35,7 +34,7 @@ class Comment(models.Model):
     approved = models.BooleanField(default=False)
 
     class Meta:
-        ordering = ['create_on']
+        ordering = ["create_on"]
 
     def __str__(self):
         return f"Comment {self.body} by {self.name}"
