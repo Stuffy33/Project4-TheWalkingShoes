97f0c0b3cc2585e8d6de1e305bd9cf590e2e5035
Stuffy33 <eric96an@gmail.com>
2022-09-08 20:46:11 +0000

    add a time out for messages

--- thewalkingshoes/settings.py
+++ thewalkingshoes/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url #Database URL library
+from django.contrib.messages import constants as messages
 if os.path.isfile("env.py"): #env.py is not found since its in gitignore with this condition it prevents an error.
     import env
 
@@ -57,6 +58,14 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL= '/'
 LOGOUT_REDIRECT_URL= '/'
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
