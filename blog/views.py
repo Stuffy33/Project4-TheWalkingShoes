ca6f795d0c5df231ca98354e29484df367981411
Stuffy33 <eric96an@gmail.com>
2022-09-08 20:27:50 +0000

    fix small bug with 2 hearts(likes) showing

--- blog/views.py
+++ blog/views.py
@@ -11,6 +11,7 @@ class PostList(generic.ListView):
     template_name = "index.html"
     paginate_by = 6
 
+
 class PostDetail(View):
 
     def get(self, request, slug, *args, **kwargs):
@@ -29,11 +30,12 @@ class PostDetail(View):
                 "comments": comments,
                 "commented": False,
                 "liked": liked,
-                "comment_form": CommentForm(),
+                "comment_form": CommentForm()
             },
         )
-
+    
     def post(self, request, slug, *args, **kwargs):
+
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.filter(approved=True).order_by("-created_on")
@@ -42,7 +44,6 @@ class PostDetail(View):
             liked = True
 
         comment_form = CommentForm(data=request.POST)
-
         if comment_form.is_valid():
             comment_form.instance.email = request.user.email
             comment_form.instance.name = request.user.username
@@ -59,18 +60,18 @@ class PostDetail(View):
                 "post": post,
                 "comments": comments,
                 "commented": True,
-                "liked": liked,
-                "comment_form": CommentForm(),
+                "comment_form": comment_form,
+                "liked": liked
             },
         )
+
 class PostLike(View):
-
-    def post(self, request, slug):
+    
+    def post(self, request, slug, *args, **kwargs):
         post = get_object_or_404(Post, slug=slug)
-
-        if post.likes.filter(id=request.user.id).exsist():
+        if post.likes.filter(id=request.user.id).exists():
             post.likes.remove(request.user)
         else:
             post.likes.add(request.user)
-        
-        return HttpResponseRedirect(reverse('post_detail',args=[slug]))
+
+        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
