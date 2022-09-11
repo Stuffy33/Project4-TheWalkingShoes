b0a2b65a80570075d3afe3da32484e17a21a9cbc
Stuffy33 <eric96an@gmail.com>
2022-09-11 06:58:34 +0000

    app didn't want to launch fixed

--- shoes/urls.py
+++ shoes/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('shoess/', views.ShoessPage.as_view(), name='shoess'),
-    path('submitshoe/', views.submit_shoes, name='submit_shoe'),
+    path('submitshoes/', views.submit_shoes, name='submit_shoe'),
     path('edit/<slug:slug>', views.edit_shoes, name='edit_shoe'),
     path('delete/<slug:slug>', views.delete_shoes, name='delete_shoe'),
     path('<slug:slug>/', views.ShoesDetails.as_view(), name='shoes_detail'),
